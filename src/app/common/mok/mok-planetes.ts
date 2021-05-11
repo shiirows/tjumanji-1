c40f0572353ef71bc5403f76c4581ceaea393fdc
Loic699 <loicpimet@gmail.com>
2021-05-11 13:59:21 +0200

    add planetes

--- src/app/common/mok/mok-planetes.ts
+++ src/app/common/mok/mok-planetes.ts
@@ -25,5 +25,45 @@ export const defaultPlanetes: Planete[]=[
         2,
        ""
       ),
+      new Planete(
+        'Mercure',
+        'https://www.stelvision.com/astro/wp-content/uploads/2019/10/Mercury_false_colors_credit_Nasa.jpg',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Jupiter',
+        'https://img.over-blog-kiwi.com/2/19/04/57/20170519/ob_3a5ffa_jupiter.jpg',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Saturne',
+        'https://lesavoirperdudesanciens.com/wp-content/uploads/2018/10/187854.jpg',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Uranus',
+        'https://img.over-blog-kiwi.com/2/19/04/57/20170520/ob_0ed193_anneaux-uranus-w-600.jpg',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Neptune',
+        'https://i.ytimg.com/vi/DIXKrRBX72E/maxresdefault.jpg',
+        
+        2,
+        2,
+       ""
+      ),
 
 ]
