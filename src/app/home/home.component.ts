e9300f737981ecd48d3fc8376182e835a026bacf
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-05-11 16:42:12 +0200

    version 16h42

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,9 +14,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.news.push(new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'Nouvel inscrit', 'Un utilisateur est arrivé sur la plateforme'),
-    new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'Nouvel inscrit', 'Un utilisateur est arrivé sur la plateforme'),
-    new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'Nouvel inscrit', 'Un utilisateur est arrivé sur la plateforme'),)
+    this.news.push(new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'Odyssey', 'j\'aime les longues balade en mer'),
+    new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'Reconnaissance', 'je suis connu pour pour mes tartes marsienne'),
+    new News('https://i.pinimg.com/originals/61/de/d9/61ded9f535761631957d6ae12326a957.jpg', 'maven', 'j\'aime la nature et les longues balades interstellaire '),)
   }
 
 }
