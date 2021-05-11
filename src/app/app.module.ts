c39a739356f81d0616e35caafab9869ae097ee2d
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-05-11 12:40:39 +0200

    version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { Routes } from '@angular/router';
 import { Page404Component } from './page404/page404.component';
 
 const ROUTES: Routes = [
   { path: 'home', component: AppComponent },
+  { path: '' , component: Page404Component}
 ];
-=======
-import { HomeComponent } from './home/home.component';
->>>>>>> lucas
 
 @NgModule({
 
   declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    Page404Component
-=======
-    HomeComponent
->>>>>>> lucas
+    AppComponent
   ],
   imports: [
     BrowserModule,
