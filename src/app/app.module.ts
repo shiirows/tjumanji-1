2a30816a7a0a56af33c01665313b6f7daac212e6
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-05-11 11:05:27 +0200

    version router 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+import { Page404Component } from './page404/page404.component';
+
+const ROUTES: Routes = [
+  { path: 'home', component: AppComponent },
+];
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
