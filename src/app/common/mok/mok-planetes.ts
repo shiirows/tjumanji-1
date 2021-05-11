953ad10868b24c9cec1741cbc3771011a620572a
Loic699 <loicpimet@gmail.com>
2021-05-11 13:40:40 +0200

    add mok & planet model

---
+++ src/app/common/mok/mok-planetes.ts
@@ -0,0 +1,29 @@
+import { Planete } from "../model/modele-planete";
+
+export const defaultPlanetes: Planete[]=[
+    new Planete(
+        'Mars',
+        'https://us.123rf.com/450wm/forplayday/forplayday1611/forplayday161100108/69342962-mars-high-resolution-beautiful-art-presents-planet-of-the-solar-system-.jpg?ver=6',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Terre',
+        'https://i.pinimg.com/originals/17/c4/85/17c48583a29a4d5c7270125c533efff9.jpg',
+        
+        2,
+        2,
+       ""
+      ),
+      new Planete(
+        'Venus',
+        'https://i.pinimg.com/originals/47/1a/fc/471afc78d2f4de837c5386f3524b212f.png',
+        
+        2,
+        2,
+       ""
+      ),
+
+]
