a05622fd579ca50eff7b4f08026d0db69311449e
Loic699 <loicpimet@gmail.com>
2021-05-12 02:23:12 +0200

    push carousel

--- src/app/common/models/modele-planete.ts
+++ src/app/common/models/modele-planete.ts
@@ -2,14 +2,14 @@ export class Planete {
 
     public name: string
     public img: string
-   
     public density: number
     public gravity: number
     public aroundPlanet: string
-    constructor(name: string,img: string,  density: number, gravity: number, aroundPlanet: string){
+    public distanceMars: string
+    constructor(name: string,img: string,  density: number, gravity: number, aroundPlanet: string, distanceMars: string){
         this.name=name
         this.img=img
-        
+        this.distanceMars=distanceMars
         this.density=density
         this.gravity=gravity
         this.aroundPlanet=aroundPlanet
